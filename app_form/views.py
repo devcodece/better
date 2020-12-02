0dca30b0d86e67dd5163a2ca078ca0c430a0f625
carlitox <carlitox@mail.salud.gob.sv>
2020-12-02 14:54:42 -0600

    Se probo la insercion con 2 modelos realcionados en forms.py y usando createview

--- app_form/views.py
+++ app_form/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic import CreateView, TemplateView
-from . forms import ProductSkuProductForm
+from . forms import TdtProductForm, TdtSkuProductForm, ProductSkuProductForm
 from django.urls import reverse
-from . models import TdtProduct
+from . models import TdtProduct, TdtSkuProduct
+from django.forms import BaseInlineFormSet, inlineformset_factory
 
 # Create your views here.
 
 class NewProduct(CreateView):
-    model = TdtProduct
+    #model = TdtProduct
     form_class = ProductSkuProductForm
     template_name = 'form-product.html'
 
-    def from_valid(self, form):
+    def form_valid(self, form):
         product = form['product'].save()
         sku = form['sku'].save(commit=False)
-        sku.product = product
+        #print(product + '====================')
+        sku.id_product = product
         sku.save()
         return HttpResponseRedirect(reverse('success'))
+
+#def create(request):
+    #TdtProductInLineFormSet = inlineformset_factory(TdtProduct, TdtSkuProduct, form=TdtProductForm)
+    #productForm = TdtProductForm(request.POST)
+
+
+
+
+
 
 class Success(TemplateView):
     template_name = 'success.html'
